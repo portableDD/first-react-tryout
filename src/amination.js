64b49efc5571afa1bf60a6b6654be18ed4afb06e
portableD <emmanueltemitopedorcas20@gamil.com>
2020-09-03 17:31:24 +0100

    navbar and working on amination

---
+++ src/amination.js
@@ -0,0 +1,76 @@
+import React from "react"
+
+function Amination() {
+    let slideIndex = 0;
+    showSlides();
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("slide-show");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style = {display: "none"};  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }  
+        slides[slideIndex-1].style = {display: "flex"};
+        setTimeout(showSlides, 10000); // Change image every 2 seconds
+    }
+    return (
+      <div className="slide-show-container">
+				<div className="slide-show slide-1" style={{display: "none"}}>
+                    <img 
+                        className="slide-img-background" 
+                        src="animi pics/slide 1/slide-background.png" 
+                        alt="banner"
+                    />
+					<div className="slide-product">
+                        <img src="animi pics/slide 1/slide-shoe1.png" alt="slides"/>
+                    </div>
+					<div className="info">
+						<span>DISCOUNT<br/>FOR COMFORT</span>
+						<span>Get your footwears at 20% off</span>
+						<a href="#">shop now</a>
+					</div>
+				</div>
+
+				<div className="slide-show slide-2" style={{display:"none"}}>
+                    <img 
+                        className="slide-img-background" 
+                        src="animi pics/slide 2/slide-background2.jpg" 
+                        alt="banner"
+                    />
+					<div className="slide-product">
+                        <img src="animi pics/slide 2/slide-shoe2.png" alt="slides"/>
+                    </div>
+					<div className="info">
+						<span>BESPOKE<br/>FOOTWEARS</span>
+						<span>Your feet deserves the best</span>
+						<a href="#">shop now</a>
+					</div>
+				</div>
+
+				<div className="slide-show slide-3" style={{display: "none"}}>
+                    <img 
+                        className="slide-img-background" 
+                        src="animi pics/slide 3/slide-background3.png" 
+                        alt="banner"
+                    />
+					<div className="slide-product">
+                        <img src="animi pics/slide 3/slide-shoe3.png" alt="slides"/>
+                    </div>
+					<div className="info">
+						<span>LOOK<br/>DIFFERENT</span>
+						<span>The confidence you need to walk the world</span>
+						<a href="#">shop now</a>
+					</div>
+				</div>
+
+				<div className="slide-show slide-4" style={{display: "flex"}}>
+					<img className="slide-img-background" src="animi pics/slide 4/slide-background4.jpg" alt="banner"/>
+				</div>
+			</div>
+    )
+} 
+
+export default Amination
---
+++ src/header.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+function Header() {
+    return (
+        
+        <header>
+            <div className="site-header">
+                <div className = "site-logo">
+                    <a href= "index.html">
+                        <img src= "logo.oni.png" alt= "the logo"/>
+                    </a>
+                </div>
+                <nav className="site-menu">
+                    
+                    <i className= "search">
+                        <input type="text" id= "try" value="search"/>
+                        <button>Submit</button>
+                    </i>
+
+                    <ul>
+                        <li><a href ="index.html">Home</a></li>
+                        <li><a href ="shoes.html">Shoes</a></li>
+                        <li><a href ="sandal.html">Sandals</a></li>
+                        <li><a href ="slipper.html">Slippers</a></li>
+                        <li><a href ="contact.html">Contact Us </a></li>
+                        <li><a href ="about.html">About Us</a></li>
+                    </ul>
+
+                </nav>
+            </div>
+            <div className = "toggle-menu">
+                
+                <span></span>
+            
+            </div>
+            
+        </header>
+    )
+}
+
+export default Header
